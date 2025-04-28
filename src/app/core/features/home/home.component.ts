8821a982b921729fdf3518ef3ba52a1e64c3b1ab
Amar <arampers94@gmail.com>
2025-04-28 18:39:58 -0400

    added lists to home screen

--- src/app/core/features/home/home.component.ts
+++ src/app/core/features/home/home.component.ts
@@ -1,11 +1,68 @@
 import { Component } from '@angular/core';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzInputModule } from 'ng-zorro-antd/input';
+import { ListComponent } from '../../../shared/ui/list/list.component';
+import { ListData } from '../../interfaces';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 
 @Component({
   selector: 'app-home',
-  imports: [NzInputModule, NzIconModule],
+  imports: [NzInputModule, NzIconModule, ListComponent, NzDividerModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public currentOOOListData: ListData[] = [
+    {
+      title: 'John Doe',
+      avatar: 'https://joesch.moe/api/v1/random',
+      description: 'Returns tomorrow',
+    },
+    {
+      title: 'Jane Doe',
+      avatar: 'https://joesch.moe/api/v1/random',
+      description: 'Returns in 3 days (Wednesday, April 30th)',
+    },
+    {
+      title: 'Amar Rampersaud',
+      avatar: 'https://joesch.moe/api/v1/random',
+      description: 'Returns in 7 days (Monday, May 5th)',
+    },
+  ];
+
+  public upcomingOOOListData: ListData[] = [
+    {
+      title: 'John Doe',
+      avatar: 'https://joesch.moe/api/v1/random',
+      description: 'Out starting tomorrow',
+    },
+    {
+      title: 'Jane Doe',
+      avatar: 'https://joesch.moe/api/v1/random',
+      description: 'Out starting in 3 days (Wednesday, April 30th)',
+    },
+    {
+      title: 'Amar Rampersaud',
+      avatar: 'https://joesch.moe/api/v1/random',
+      description: 'Out starting in 7 days (Monday, May 5th)',
+    },
+  ];
+
+  public teamMembersListData: ListData[] = [
+    {
+      title: 'Amar Rampersaud',
+      avatar: 'https://joesch.moe/api/v1/random',
+      description: 'Working today',
+    },
+    {
+      title: 'Matt Collins',
+      avatar: 'https://joesch.moe/api/v1/random',
+      description: 'Working today',
+    },
+    {
+      title: 'Mike Lightner',
+      avatar: 'https://joesch.moe/api/v1/random',
+      description: 'Working today',
+    },
+  ];
+}
