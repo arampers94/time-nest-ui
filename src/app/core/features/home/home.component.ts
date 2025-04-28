bce3d80f4748e8e4799b67a6a540b3018a19b24e
Amar <arampers94@gmail.com>
2025-04-28 15:20:07 -0400

    added search bar to home page

--- src/app/core/features/home/home.component.ts
+++ src/app/core/features/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NzInputModule, NzIconModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
