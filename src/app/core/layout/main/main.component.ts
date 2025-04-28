edb9330b48b18fc92393d3f33c4cd65023dc0689
Amar <arampers94@gmail.com>
2025-04-28 13:57:48 -0400

    renamed ui folder to layout. added body component

--- src/app/core/layout/main/main.component.ts
+++ src/app/core/layout/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { BodyComponent } from '../body/body.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, BodyComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
