63609b665355a7fc4a81f485d763050b2193bc3d
Amar <arampers94@gmail.com>
2025-04-28 20:49:13 -0400

    set up calendar ui

--- src/app/core/features/calendar/calendar.component.ts
+++ src/app/core/features/calendar/calendar.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NzCalendarModule } from 'ng-zorro-antd/calendar';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
+  imports: [NzPageHeaderModule, NzCalendarModule, FormsModule, NzPopoverModule],
   templateUrl: './calendar.component.html',
-  styleUrl: './calendar.component.scss'
+  styleUrl: './calendar.component.scss',
 })
 export class CalendarComponent {
+  public date: Date = new Date();
 
+  public onDateChange(event: any): void {
+    console.log('date changed', event);
+  }
 }
