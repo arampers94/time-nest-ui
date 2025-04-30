1d895647a5883870732a1772b2b2f7fb97c99067
Amar <arampers94@gmail.com>
2025-04-30 18:44:05 -0400

    store helpers

---
+++ src/app/store/helpers/store-result.ts
@@ -0,0 +1,38 @@
+import { HttpErrorResponse } from "@angular/common/http"
+import { handleError } from "../../core/helpers"
+import { ApiError } from "../../core/models"
+
+
+export type SuccessResult = {
+  status: 'success'
+}
+
+export type FailureResult = {
+  status: 'failure',
+  error: ApiError
+}
+
+export type ActionResult = SuccessResult | FailureResult | null;
+
+export function createValueSuccess<State, K extends keyof State>(
+  valuePropName: K,
+  value: State[K],
+  resultPropName: keyof State
+): Partial<State> {
+  return {
+    [valuePropName]: value,
+    [resultPropName]: { status: 'success' } as SuccessResult
+  } as Partial<State>
+}
+
+export function createValueFailure<State>(
+  error: HttpErrorResponse,
+  resultPropName: keyof State
+): Partial<State> {
+  return {
+    [resultPropName]: {
+      status: 'failure',
+      error: handleError(error)
+    } as FailureResult
+  } as Partial<State>;
+}
