2061b0e57668ce5c0011aab104a285abfc637540
Amar <arampers94@gmail.com>
2025-05-01 22:27:26 -0400

    added display for empty lists

--- src/app/shared/ui/list/list.component.ts
+++ src/app/shared/ui/list/list.component.ts
@@ -1,13 +1,15 @@
 import { Component, input } from '@angular/core';
 import { NzListModule } from 'ng-zorro-antd/list';
 import { ListData } from '../../../core/interfaces';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
 
 @Component({
   selector: 'app-list',
-  imports: [NzListModule],
+  imports: [NzListModule, NzEmptyModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
 export class ListComponent {
-  listData = input<ListData[]>();
+  public listData = input<ListData[]>();
+  public noContentText = input<string>('No data available');
 }
