f8d059e44eaa10084cec1715fbee768cce664dac
Amar <arampers94@gmail.com>
2025-05-07 02:13:46 -0400

    added menu items and styling

--- src/app/core/features/menu/menu.component.ts
+++ src/app/core/features/menu/menu.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TeamSelectComponent } from '../../../shared/ui/team-select/team-select.component';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+interface MenuItem {
+  id: number;
+  title: string;
+  icon: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-menu',
-  imports: [TeamSelectComponent],
+  imports: [TeamSelectComponent, NzIconModule, CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
-export class MenuComponent {}
+export class MenuComponent {
+  public router = inject(Router);
+  public menuItems: MenuItem[] = [
+    {
+      id: 1,
+      title: 'Home',
+      icon: 'home',
+      link: '/home',
+    },
+    {
+      id: 2,
+      title: 'Calendar',
+      icon: 'calendar',
+      link: '/calendar',
+    },
+    {
+      id: 3,
+      title: 'Favorites',
+      icon: 'star',
+      link: '/favorites',
+    },
+  ];
+
+  public selectedItemId: number = 1;
+
+  public onClick(item: MenuItem): void {
+    this.selectedItemId = item.id;
+    this.router.navigate([item.link]);
+  }
+}
