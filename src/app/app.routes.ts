899dc66f90a8c4768793af324c5eef893b1fe29f
Amar <arampers94@gmail.com>
2025-05-07 01:40:41 -0400

    refactored components to be loaded via routing instead of tabs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,30 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './core/layout/main/main.component';
+import { CalendarComponent } from './core/features/calendar/calendar.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainComponent,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./core/features/home/home.component').then(
+            (m) => m.HomeComponent
+          ),
+      },
+      {
+        path: 'calendar',
+        component: CalendarComponent,
+      },
+      {
+        path: 'favorites',
+        loadComponent: () =>
+          import('./core/features/favorites/favorites.component').then(
+            (m) => m.FavoritesComponent
+          ),
+      },
+    ],
   },
 ];
--- src/app/core/layout/body/body.component.ts
+++ src/app/core/layout/body/body.component.ts
@@ -1,46 +1,10 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from '../../features/home/home.component';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzTabsModule } from 'ng-zorro-antd/tabs';
-import { CalendarComponent } from '../../features/calendar/calendar.component';
-import { FavoritesComponent } from '../../features/favorites/favorites.component';
-import { NgComponentOutlet } from '@angular/common';
-
-interface Tab {
-  title: string;
-  icon: string;
-  component: any;
-}
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-body',
-  imports: [NzTabsModule, NzIconModule, NgComponentOutlet],
+  imports: [RouterOutlet],
   templateUrl: './body.component.html',
   styleUrl: './body.component.scss',
 })
-export class BodyComponent {
-  public HomeComponent = HomeComponent;
-  public CalendarComponent = CalendarComponent;
-  public FavoritesComponent = FavoritesComponent;
-  public tabs: Tab[] = [
-    {
-      title: 'Home',
-      icon: 'home',
-      component: this.HomeComponent,
-    },
-    {
-      title: 'Calendar',
-      icon: 'calendar',
-      component: this.CalendarComponent,
-    },
-    {
-      title: 'Favorites',
-      icon: 'star',
-      component: this.FavoritesComponent,
-    },
-  ];
-
-  public onTabChange(event: any): void {
-    console.log('Tab changed:', event);
-  }
-}
+export class BodyComponent {}
