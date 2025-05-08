bed667a71c7c374cb6ccc80ee94c96b6f4ab4abd
Amar <arampers94@gmail.com>
2025-05-07 23:06:56 -0400

    display users with time off events in calendar

--- src/app/core/features/calendar/calendar.component.ts
+++ src/app/core/features/calendar/calendar.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NzCalendarModule } from 'ng-zorro-antd/calendar';
 import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 import { NzPopoverModule } from 'ng-zorro-antd/popover';
+import { TeamsStore } from '../../../store/teams.store';
+import { TimeOffEvent } from '../../interfaces';
+import { TimeOffEventsStore } from '../../../store';
 
 @Component({
   selector: 'app-calendar',
@@ -12,8 +15,48 @@ import { NzPopoverModule } from 'ng-zorro-antd/popover';
 })
 export class CalendarComponent {
   public date: Date = new Date();
+  public previousDate: Date = new Date();
+  public selectedTeam = inject(TeamsStore).selectedTeam;
+  public timeOffEventsStore = inject(TimeOffEventsStore);
+  public calendarTimeOffEvents: TimeOffEvent[] = [];
+  public teamId: number | null = null;
 
-  public onDateChange(event: any): void {
-    console.log('date changed', event);
+  constructor() {
+    effect(() => {
+      this.calendarTimeOffEvents =
+        this.timeOffEventsStore.calendarTimeOffEvents();
+    });
+
+    effect(() => {
+      if (this.selectedTeam()) {
+        this.teamId = this.selectedTeam()?.id!;
+        this.getCalendarTimeOffEvents();
+      }
+    });
+  }
+
+  public onDateChange(event: Date): void {
+    if (
+      this.previousDate.getMonth() !== event.getMonth() ||
+      this.previousDate.getFullYear() !== event.getFullYear()
+    ) {
+      this.getCalendarTimeOffEvents();
+    }
+
+    this.previousDate = this.date;
+  }
+
+  public isDateWithinEventRange(date: Date, event: TimeOffEvent): boolean {
+    const startDate = new Date(event.start_date);
+    const endDate = new Date(event.end_date);
+    return date >= startDate && date <= endDate;
+  }
+
+  private getCalendarTimeOffEvents(): void {
+    this.timeOffEventsStore.getCalendarTimeOffEventsByTeamId({
+      teamId: this.selectedTeam()?.id!,
+      month: this.date.getMonth() + 1,
+      year: this.date.getFullYear().toString(),
+    });
   }
 }
